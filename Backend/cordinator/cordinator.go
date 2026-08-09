c8855f061e8489322ed0a342d23e32b35ae789f5
Sawastik Bhullar <sawastik7@gmail.com>
2026-08-09 09:52:13 +0530

    successfully fetched my first response body and read the bytes from it using io.Readall method

--- Backend/cordinator/cordinator.go
+++ Backend/cordinator/cordinator.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
@@ -17,13 +19,33 @@ var chunkLocations = make(map[string]string)
 
 var nextNodeIndex = 0
 
+
+
 func main() {
 
 	port:= flag.String("port","9000","coordinator port");
 
 	flag.Parse();
 	mux:=http.NewServeMux();
+// first we need to build a put route here, in which we have to read a body for this 
 
+mux.HandleFunc("PUT /files/{filename}",func(w http.ResponseWriter, r *http.Request) {
+ 
+	        data,err:=io.ReadAll(r.Body);
+			if err!=nil{
+				fmt.Println(err);
+				return;
+			}
+
+			fmt.Println(string(data))
+
+			
+			
+
+			
+
+		w.WriteHeader(http.StatusOK);
+})
 
 
 	log.Fatal(http.ListenAndServe(":"+*port,mux));
